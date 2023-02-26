e5daf4ce7e4065cc4cf795fd9170f1995df5ab8f
Maks <maks-lug333@yandex.ru>
2023-02-26 22:03:47 +0900

    Ok

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+int x = int.Parse(Console.ReadLine());
+Console.Write($"Сумма чисел {x} = {GetSum(x)}");
+
+int GetSum(int x)
+{
+   
+        int sum = 0;
+        while (x > 0)
+        {
+            sum += x%10;
+            x = x/10;
+        }
+        return sum;
+}
